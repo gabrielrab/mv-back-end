229b118d9c05fe44a8e93f50efbcd07f815b3b57
Gabriel Rabelo Camargos <rabelogabriel72@gmail.com>
2019-12-31 14:09:36 -0300

    Auth route

--- src/routes.js
+++ src/routes.js
@@ -14,6 +14,8 @@ routes.get("/user/:id", UserController.show);
 routes.post("/user", UserController.store);
 routes.put("/user/:id", UserController.update);
 routes.delete("/user/:id", UserController.destroy);
+//Login
+routes.post("/auth", UserController.authenticate);
 //Order
 routes.get("/order", OrderController.index);
 routes.get("/order/:id", OrderController.show);
